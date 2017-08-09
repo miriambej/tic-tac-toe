eaf4cf881afff8bd033ef922b63319625a10f73a
Miriam Espinosa Jorrin <miriam@192-168-1-2.tpgi.com.au>
2017-08-09 22:21:26 +1000

    project knows when player is a winner or withdraw but it is not linked to the dom yet

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 //logic code
 tictactoe = {
 
-  players : [ { player:0, name: 'p1', image:"images/x.png" }, { player:1 , name: 'p2', image:"images/o.png" } ],
+  players : [ { player:0, name: 'O', image:"./images/x.png", score:0}, { player:1 , name: 'X', image:"./images/o.png", score:0 } ],
 
   switcher:0,
 
@@ -9,28 +9,65 @@ tictactoe = {
 
   board : ["_","_","_","_","_","_","_","_","_"],
 
+  alertWinMessage(){
+    setTimeout(function(){
+      tictactoe.players[tictactoe.switcher].score++
+      console.log(tictactoe.players[tictactoe.switcher].score);
+      alert(`The winner is ${tictactoe.players[tictactoe.switcher].name}`)
+      tictactoe.reset()
+    }, 150 )
+  },
+
+  alertDrawMessage(){
+    setTimeout(function(){
+      alert('Nobody wins!')
+    }, 150 )
+  },
+
   play(player, x) {
-    if (tictactoe.board[x] === "_"){
-      tictactoe.board[x] = player
-      tictactoe.turns++
+    if (this.board[x] === "_"){
+      this.board[x] = player
+      this.turns++
+      this.winner(player);
       return "played";
+
     }
   },
 
+  reset(){
+    $(".top").attr('src', "images/top.jpg")
+    this.board = ["_","_","_","_","_","_","_","_","_"]
+    $(".top").removeAttr('clicked')
+    this.turns = 0;
+  },
+
+  // clearBoard(){
+  //   return document.getElementById('id').innerHTML = "./images/top.jpg";
+  // },
+
   winner(player) {
+
+
     if (
-      (tictactoe.board[0]===player) && (tictactoe.board[1]===player) && (tictactoe.board[2]===player) || //horizontal
-      (tictactoe.board[3]===player) && (tictactoe.board[4]===player) && (tictactoe.board[5]===player) || //horizontal
-      (tictactoe.board[6]===player) && (tictactoe.board[7]===player) && (tictactoe.board[8]===player) || //horizontal
-      (tictactoe.board[0]===player) && (tictactoe.board[3]===player) && (tictactoe.board[6]===player) || //vertical
-      (tictactoe.board[1]===player) && (tictactoe.board[4]===player) && (tictactoe.board[7]===player) || //vertical
-      (tictactoe.board[2]===player) && (tictactoe.board[5]===player) && (tictactoe.board[8]===player) || //vertical
-      (tictactoe.board[0]===player) && (tictactoe.board[4]===player) && (tictactoe.board[8]===player) || //diagonal
-      (tictactoe.board[2]===player) && (tictactoe.board[4]===player) && (tictactoe.board[6]===player)  //diagonal
+      (this.board[0]===player) && (this.board[1]===player) && (this.board[2]===player) || //horizontal
+      (this.board[3]===player) && (this.board[4]===player) && (this.board[5]===player) || //horizontal
+      (this.board[6]===player) && (this.board[7]===player) && (this.board[8]===player) || //horizontal
+      (this.board[0]===player) && (this.board[3]===player) && (this.board[6]===player) || //vertical
+      (this.board[1]===player) && (this.board[4]===player) && (this.board[7]===player) || //vertical
+      (this.board[2]===player) && (this.board[5]===player) && (this.board[8]===player) || //vertical
+      (this.board[0]===player) && (this.board[4]===player) && (this.board[8]===player) || //diagonal
+      (this.board[2]===player) && (this.board[4]===player) && (this.board[6]===player)  //diagonal
       ){
-      console.log("you won");
-    }else if (tictactoe.turns === 9) {
-      console.log("It is a draw");
+      //alert("you won");
+      this.alertWinMessage();
+      //this.reset();
+      // t = setTimeout("tictactoe.reset()",2000);
+      // this.clearBoard();
+
+
+    }else if (this.turns === 9) {
+      this.alertDrawMessage();
+      //this.reset();
     }
   }
 
@@ -48,25 +85,40 @@ const render = function (){
 $(document).ready(function(){
 
   $('img').on('click', function() {
-  console.log($(this));
-  $(this).context.src = tictactoe.players[tictactoe.switcher].img
-  // tictactoe.play()
-  console.log(tictactoe.players[tictactoe.switcher].name);
-  tictactoe.winner()
-  console.log("clicked");
-  if(tictactoe.switcher === 0){
-  tictactoe.switcher = 1
-}else{
-  tictactoe.switcher = 0
-}
+    console.log($(this));
+    //$(this).context.src = tictactoe.players[tictactoe.switcher].img
+    if($(this).attr('clicked' )) {
+      return;
+    }
+    $(this).attr('clicked', 'true' )
+    $(this).attr('src', tictactoe.players[tictactoe.switcher].image)
+
+    // tictactoe.play()
+    console.log(tictactoe.players[tictactoe.switcher].name);
+
+    console.log("clicked");
+    const index = $(this).attr('id');
+    if(tictactoe.switcher === 0){
+      tictactoe.switcher = 1;
+      tictactoe.play('p1', index);
+    }else{
+      tictactoe.switcher = 0
+      tictactoe.play('p2', index);
+    }
+    console.log(tictactoe.board);
 
   });
 
 
-  // $('img').on('click', function() {
-  // console.log($(this));
-  // $(this).context.src = "images/o.png"
-  // console.log("clicked");
-  // });
+
+  let $button = document.querySelector('.button');
+  $button.addEventListener('click', function() {
+  let duration = 0.3,
+      delay = 0.08;
+  TweenMax.to($button, duration, {scaleY: 1.6, ease: Expo.easeOut});
+  TweenMax.to($button, duration, {scaleX: 1.2, scaleY: 1, ease: Back.easeOut, easeParams: [3], delay: delay});
+  TweenMax.to($button, duration * 1.25, {scaleX: 1, scaleY: 1, ease: Back.easeOut, easeParams: [6], delay: delay * 3 });
+  tictactoe.reset();
+  });
 
 });
